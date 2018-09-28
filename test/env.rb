00a054219529f9908f27b0b0c76fb673a54178b0
David Dugue <ddugue@kumoweb.ca>
2018-09-28 17:01:49 -0400

    Added more test to env

--- test/env.rb
+++ test/env.rb
@@ -5,6 +5,9 @@ require 'rake/garden/env_parser'
 describe Garden::EnvParser do
   retour = {
     FaKe_DEBUG: 'Yes',
+    FaKe_DEBUG_1: 'Y',
+    FaKe_DEBUG_2: 'False',
+    number: '2',
     PRODUCTION: 'node'
   }
   before(:each) do
@@ -17,5 +20,22 @@ describe Garden::EnvParser do
   context 'with strings' do
     let(:key) { 'PRODUCTION' }
     it { is_expected.to eq('node') }
+  end
+
+  context 'with booleans' do
+    let(:key) { 'FAKE_DEBUG' }
+    it { is_expected.to eq(true) }
+    describe "Fake_debug_1" do
+      let(:key) { 'fake_debug_1' }
+      it { is_expected.to eq(true) }
+    end
+    describe "Fake_debug_2" do
+      let(:key) { 'fake_debug_2' }
+      it { is_expected.to eq(false) }
+    end
+  end
+  context 'with number' do
+      let(:key) { 'number' }
+      it { is_expected.to eq(2) }
   end
 end
