acc76f86c7543508469e43609f149675fa36dab5
David Dugue <ddugue@kumoweb.ca>
2018-10-10 17:07:02 -0400

    Cleaneu pd and added test for file aware string

--- lib/rake/garden/fileawarestring.rb
+++ lib/rake/garden/fileawarestring.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
 
 module Garden
+  ## String that can substitue part of itself to repla
   class FileAwareString
     attr_reader :file
-    attr_reader :folder_root
+    attr_reader :directory_root
 
-    def initialize(folder_root, file, string)
-      @folder_root = folder_root
+    def initialize(directory_root, file, string)
+      @directory_root = directory_root || "#{File.dirname(file)}/"
       @file = file
       @string = string
 
@@ -14,32 +16,46 @@ module Garden
       @formatted = false
     end
 
-    def prefix
-      @prefix ||=
-        @folder_root ? @file.to_s.sub(@folder_root, '').sub(File.basename(@file), '') : ''
+    # Return the directory part of the path minus the directory root
+    # Allows substitution in relative paths
+    def directory_prefix
+      @directory_prefix ||= @file.to_s\
+                                 .sub(@directory_root, '')\
+                                 .sub(File.basename(@file), '')
     end
 
-    ##
     # Return wether this string is a glob pattern
     def glob?
+      @string.include? '*'
     end
 
+    # Format the string if it hasn't been formatted by replacing some special
+    # character expression by part of the file
+    # %f - Replace with file basename and relative directory pathy
+    # %F - Replace ONLY with file basename
+    # %b - Replace with file basename without extension and relative directory
+    # %B - Replace only with file base name without extension
+    # %x - Replace with file extension
+    # %d - Replace with relative directory path
+    # %D - Replace with full directory path
+    # %p - Replace with full path
     def format
       return @string if @formatted
       @formatted = true
       @string.gsub!(/%[bBfFpxdD]/) do |s|
         case s.to_s
-        when '%f' then prefix + File.basename(@file)
+        when '%f' then directory_prefix + File.basename(@file)
         when '%F' then File.basename(@file)
-        when '%b' then prefix + File.basename(@file, '.*')
+        when '%b' then directory_prefix + File.basename(@file, '.*')
         when '%B' then File.basename(@file, '.*')
         when '%x' then File.extname(@file)
-        when '%d' then prefix.empty? ? "#{File.dirname(@file)}/" : prefix
+        when '%d' then
+          directory_prefix.empty? ? @directory_root : directory_prefix
         when '%D' then File.dirname(@file)
         when '%p' then @file
         end
       end
-      return @string
+      @string
     end
 
     def to_s
@@ -54,26 +70,28 @@ module Garden
 
     class << self
       attr_accessor :file
-      attr_accessor :folder_root
+      attr_accessor :directory_root
 
-      # def with_context(file, folder_root)
-      #   if folder_root
-      #     previous_root = self.folder_root
-      #     self.folder_root = folder_root
-      #   end
-      #   if file
-      #     previous_file = self.folder_root
-      #     self.file = self.file
-      #   end
-      #   self.file = file
-      #   self.folder_root
-      #   yield
-      #   self.
-      # end
+      def with_file(file, directory_root = nil)
+        if directory_root
+          previous_root = self.directory_root
+          self.directory_root = directory_root
+        end
+        previous_file = self.file
+        self.file = file
+        yield
+        self.file = previous_file
+        self.directory_root = previous_root if previous_root
+      end
+
+      def with_folder(folder, &block)
+        with_file(nil, folder, &block)
+      end
+
       ##
       # Shortcut function to create a file aware string based on the current context
       def create(string)
-        new(self.folder_root, self.file, string)
+        new(self.directory_root, self.file, string)
       end
 
       def [](string)
--- test/fileawarestring.rb
+++ test/fileawarestring.rb
@@ -75,35 +75,81 @@ RSpec.describe Garden::FileAwareString, "#format" do
       it { is_expected.to eql('/home/sub/test.txt') }
     end
   end
+
+  context "with globs" do
+    subject { Garden::FileAwareString.new(nil, '/home/sub/test.txt', glob) }
+
+    describe "with double glob" do
+      let(:glob) { "/home/**/*.txt" }
+      it { is_expected.to have_attributes('glob?' => true) }
+    end
+
+    describe "with single glob" do
+      let(:glob) { "/home/abc/*.txt" }
+      it { is_expected.to have_attributes('glob?' => true) }
+    end
+    describe "without glob" do
+      let(:glob) { "/home/abc/a.txt" }
+      it { is_expected.to have_attributes('glob?' => false) }
+    end
+  end
+
+  describe "with_file" do
+    let(:file) { "/home/abc/b.txt" }
+    let(:folder) { "/home/" }
+    subject { Garden::FileAwareString }
+
+    it 'should set the folder and the filepath' do
+      filestring = nil
+      subject.with_file file, folder do
+        filestring = subject.create('')
+        expect(filestring).to have_attributes(
+                                'file' => file,
+                                'directory_root' => folder
+                              )
+      end
+
+      expect(filestring).to have_attributes(
+                              'file' => file,
+                              'directory_root' => folder
+                            )
+    end
+
+    it 'should preserve the folder even if the file change' do
+      filestring = nil
+      subject.with_folder folder do
+        subject.with_file file do
+          expect(subject.create('')).to have_attributes(
+                                          'file' => file,
+                                          'directory_root' => folder
+                                        )
+        end
+      end
+    end
+
+    it 'should override the file if the file changed' do
+      filestring = nil
+      subject.with_file 'wrong_file', folder do
+        subject.with_file file do
+          expect(subject.create('')).to have_attributes(
+                                          'file' => file,
+                                          'directory_root' => folder
+                                        )
+        end
+      end
+    end
+
+    it 'should restore the file after the file changed' do
+      filestring = nil
+      subject.with_file file, folder do
+        subject.with_file 'wrong_file' do
+        end
+
+        expect(subject.create('')).to have_attributes(
+                                        'file' => file,
+                                        'directory_root' => folder
+                                      )
+      end
+    end
+  end
 end
-
-#   it "should replace %x with the extension of the file" do
-#     expect(FileAwareString.new(nil, "/home/test.txt", "%x").to_s)\
-#       .to eq(".txt")
-#     expect(FileAwareString.new("/home/", "/home/sub/test.txt", "%x").to_s)\
-#       .to eq(".txt")
-#     expect(FileAwareString.new("/home/", "/home/sub/test", "%x").to_s)\
-#       .to eq("")
-#   end
-
-# end
-
-# RSpec.describe FileAwareString, "#creation" do
-#   it "should pass the folder root and the file from the class" do
-#     FileAwareString.file = "a.txt"
-#     FileAwareString.folder_root = "root"
-#     f = FileAwareString.create("%f")
-#     expect(f.file).to eq("a.txt")
-#     expect(f.folder_root).to eq("root")
-#   end
-
-#   it "should not override previously passed value" do
-#     FileAwareString.file = "a.txt"
-#     FileAwareString.folder_root = "root"
-#     f = FileAwareString.create("%f")
-#     FileAwareString.file = "b.txt"
-#     FileAwareString.folder_root = "root2"
-#     expect(f.file).to eq("a.txt")
-#     expect(f.folder_root).to eq("root")
-#   end
-# end
